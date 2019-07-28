c12cd6cb100fbff1f5eb1a990fb67fa34c7f516f
javier <richards.javier@gmail.com>
2019-07-27 20:45:45 -0400

    se le cambio el nombre a las tareas y sus porcentajes de avance

--- src/App.js
+++ src/App.js
@@ -8,17 +8,16 @@ import Typography from '@material-ui/core/Typography';
 import Hours from './Components/Hours'
 
 
-
 var projects = [
   {
     "id": 1,
     "name": "Fifa19",
     'tasks': [
-      { id: "1", name: 'hoña', "progress": 10 },
-      { id: "2", name: 'trabajar', "progress": 20 },
-      { id: "3", name: 'construir', "progress": 30 },
-      { id: "4", name: 'pensar', "progress": 40 },
-      { id: "5", name: 'rehacer', "progress": 50 },
+      { id: "1", name: 'hola', "progress": 100 },
+      { id: "2", name: 'esta', "progress": 40 },
+      { id: "3", name: 'es', "progress": 30 },
+      { id: "4", name: 'una', "progress": 30 },
+      { id: "5", name: 'tarea', "progress": 50 },
     ]
   },
   {
@@ -26,46 +25,47 @@ var projects = [
     "name": "StarWars",
     'tasks': [
       { id: "51", name: 'soy', "progress": 10 },
-      { id: "6", name: 'trabajar', "progress": 20 },
-      { id: "7", name: 'construir', "progress": 30 },
-      { id: "8", name: 'pensar', "progress": 40 },
-      { id: "9", name: 'rehacer', "progress": 50 },
+      { id: "6", name: 'muy', "progress": 50 },
+      { id: "7", name: 'rapido', "progress": 70 },
+      { id: "8", name: 'gracias', "progress": 40 },
+      { id: "9", name: 'a react', "progress": 50 },
     ]
   },
   {
     "id": 3,
     "name": "Pocoyo",
     'tasks': [
-      { id: "10", name: 'el', "progress": 10 },
-      { id: "11", name: 'trabajar', "progress": 20 },
-      { id: "12", name: 'construir', "progress": 30 },
-      { id: "13", name: 'pensar', "progress": 40 },
-      { id: "14", name: 'rehacer', "progress": 50 },
+      { id: "10", name: 'el', "progress": 25 },
+      { id: "11", name: 'trabajo', "progress": 20 },
+      { id: "12", name: 'consiste', "progress": 30 },
+      { id: "13", name: 'en', "progress": 90 },
+      { id: "14", name: 'pensar', "progress": 35 },
     ]
   },
   {
     "id": 4,
     "name": "Doraemon",
     'tasks': [
-      { id: "15", name: 'elemento', "progress": 10 },
-      { id: "16", name: 'trabajar', "progress": 20 },
-      { id: "17", name: 'construir', "progress": 30 },
-      { id: "18", name: 'pensar', "progress": 40 },
-      { id: "19", name: 'rehacer', "progress": 50 },
+      { id: "15", name: 'mucha', "progress": 12 },
+      { id: "16", name: 'gente', "progress": 12 },
+      { id: "17", name: 'piensa', "progress": 12 },
+      { id: "18", name: 'sin', "progress": 12 },
+      { id: "19", name: 'pensar', "progress": 12 },
     ]
   },
   {
     "id": 5,
     "name": "Lineage 3",
     'tasks': [
-      { id: "20", name: 'diseñar', "progress": 10 },
-      { id: "21", name: 'trabajar', "progress": 20 },
-      { id: "22", name: 'construir', "progress": 30 },
-      { id: "23", name: 'pensar', "progress": 40 },
-      { id: "24", name: 'rehacer', "progress": 50 },
+      { id: "20", name: 'las', "progress": 100 },
+      { id: "21", name: 'percusiones', "progress": 60 },
+      { id: "22", name: 'de', "progress": 30 },
+      { id: "23", name: 'fumar', "progress": 40 },
+      { id: "24", name: 'son malas', "progress": 50 },
     ]
   }
 ]
+
 
 class App extends React.Component {
   
