005665c216fdd7145020e276eb1b8ae28b835643
javier <richards.javier@gmail.com>
2019-08-03 11:55:54 -0400

    Se agrega lectura desde archivo json (DataBase/projects.json) y se añaden enlaces web ppara las imagenes

--- src/App.js
+++ src/App.js
@@ -7,76 +7,11 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Hours from './Components/Hours'
 import Dialog from './Components/Dialog'
-const fifa19 = require('./img/fifa19.png')
-const lineage = require('./img/lineage.png')
-const starWars = require('./img/starWars.png')
-const Pocoyo = require('./img/pocoyo.jpg')
-const Doraemon = require('./img/cosmico.png')
+import  projects from  './DataBase/projects.json'
 
 
 
-var projects = [
-  {
-    "id": 1,
-    "name": "Fifa19",
-    "img": fifa19,
-    'tasks': [
-      { id: "1", name: 'hola', "progress": 100 },
-      { id: "2", name: 'esta', "progress": 40 },
-      { id: "3", name: 'es', "progress": 30 },
-      { id: "4", name: 'una', "progress": 30 },
-      { id: "5", name: 'tarea', "progress": 50 },
-    ]
-  },
-  {
-    "id": 2,
-    "img": starWars,
-    "name": "StarWars",
-    'tasks': [
-      { id: "51", name: 'soy', "progress": 10 },
-      { id: "6", name: 'muy', "progress": 50 },
-      { id: "7", name: 'rapido', "progress": 70 },
-      { id: "8", name: 'gracias', "progress": 40 },
-      { id: "9", name: 'a react', "progress": 50 },
-    ]
-  },
-  {
-    "id": 3,
-    "img": Pocoyo,
-    "name": "Pocoyo",
-    'tasks': [
-      { id: "10", name: 'el', "progress": 25 },
-      { id: "11", name: 'trabajo', "progress": 20 },
-      { id: "12", name: 'consiste', "progress": 30 },
-      { id: "13", name: 'en', "progress": 90 },
-      { id: "14", name: 'pensar', "progress": 35 },
-    ]
-  },
-  {
-    "id": 4,
-    "img": Doraemon,
-    "name": "Doraemon",
-    'tasks': [
-      { id: "15", name: 'mucha', "progress": 12 },
-      { id: "16", name: 'gente', "progress": 12 },
-      { id: "17", name: 'piensa', "progress": 12 },
-      { id: "18", name: 'sin', "progress": 12 },
-      { id: "19", name: 'pensar', "progress": 12 },
-    ]
-  },
-  {
-    "id": 5,
-    "img": lineage,
-    "name": "Lineage 3",
-    'tasks': [
-      { id: "20", name: 'las', "progress": 100 },
-      { id: "21", name: 'percusiones', "progress": 60 },
-      { id: "22", name: 'de', "progress": 30 },
-      { id: "23", name: 'fumar', "progress": 40 },
-      { id: "24", name: 'son malas', "progress": 50 },
-    ]
-  }
-]
+
 
 
 class App extends React.Component {
